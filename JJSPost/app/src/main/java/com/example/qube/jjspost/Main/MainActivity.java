8a8d1975a3dc423a8975a22f1a9caa142f5f79b9
Jason Chan <jchan122490@gmail.com>
2016-08-15 14:24:50 -0400

    Start of Project with splash screen

---
+++ JJSPost/app/src/main/java/com/example/qube/jjspost/Main/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.qube.jjspost.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.qube.jjspost.R;
+
+public class MainActivity extends AppCompatActivity {
+/*
+Make a separate branch to work on and then pull before pushing to master when you're finished a task
+Splash screen is done
+ */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
